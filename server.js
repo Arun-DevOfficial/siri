05ab4f0e18d9827f4cea1ccdbfebbd9e75981919
Arun-DevOfficial <darunkumar1204@gmail.com>
2024-12-06 11:05:16 +0530

    updated

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.listen(7000, () => {
   console.log("server is running on port 7000");
 });
